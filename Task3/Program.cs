1363c748098296f38ec596c793cdef3b17ef906f
Sunmarine <Sunmarine777@yandex.ru>
2023-12-03 23:29:58 +0300

    Task3 array swapped

---
+++ Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 3: Напишите программу, которая перевернёт одномерный массив 
+//(первый элемент cтанет последним, второй – предпоследним и т.д.)
+
+using System;
+
+class Program
+{
+      static void Main(string[] args)
+      {
+            int size = Convert.ToInt32(10);
+            Random rand = new Random();
+            int[] array = new int[size];
+            int temp = 0;
+                  
+            for (int i = 0; i < size; i++)
+            {
+                  array[i] = rand.Next(1,50);
+                  Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+
+            
+            for (int i = 0; i < size; i++)
+            {
+            
+                  temp = array[i];
+                  array[i] = array[size - i - 1];
+                  array[i] = temp;
+                  
+                  Console.Write(array[size - i - 1] + " ");
+            }
+            
+      }
+}
