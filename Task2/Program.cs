4631f43e538635ce592d0b9ce2f988d0c77b452a
Sunmarine <Sunmarine777@yandex.ru>
2023-12-03 18:24:51 +0300

    Task2 even numbers counted

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 2: Задайте массив заполненный случайными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//Console.Write("Enter array size: ");
+//int size = Convert.ToInt32(Console.ReadLine());
+
+int size = Convert.ToInt32(900);
+int[] array = new int [size];
+Random rand = new Random();
+int countEven = 0;
+            
+for (int i = 0; i < size; i++)
+      {
+            int number = rand.Next(100,999);
+            if (number % 2 == 0)
+            countEven++;
+
+      }
+
+Console.WriteLine($"Even numbers in array: " + countEven);
